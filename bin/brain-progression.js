8feda0c6ed426e19226909327a7d3eb1c8412900
GICK00 <utinsergei7212@gmail.com>
2023-08-31 15:43:19 +0300

    Исправления опечаток

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -5,5 +5,5 @@ import game from '../src/main.js';
 
 console.log('Welcome to the Brain Games!');
 var name = greetUser();
-console.log(`Find the greatest common divisor of given numbers.`);
+console.log(`What number is missing in the progression?`);
 game(name, "progression");
